4bf86c9d2ad9448d211b8738ae89a4022ed61818
Nathan2474 <nathan.reyes.privat@gmail.com>
2020-08-14 05:38:31 +0200

    Revert "Doing stuff..."
    This reverts commit b235fdb7a5f0df931dd16c53d26b47aa9821bbef.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-![notic.js](/images/icon-1080.png)
+<img src="/icon-1080.png" width="100" height="100">
 
 # notic.js
-Another frontend framework made by a 16 year old.
+Frontend framework. Simple.
--- examples/Example app/app.js
+++ examples/Example app/app.js
@@ -1,7 +1,7 @@
 class App extends Notic.Component { //The App component.
     
     html(){ //Method that generates html of the App component.
-        var icon_1080 = `<img src = "/img/icon-1080.png" id = "icon" />`;
+        var icon_1080 = `<img src = "img/icon-1080.png" id = "icon" />`;
         var info = `<span style="font-size: 30px; opacity: 0.7;"> Source is located in /app.js.<span>`;
         return  `${icon_1080} ${info}`;     //Returns the generated html.
     }
