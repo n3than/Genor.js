613420ccc5462af1bad543418e250d58f21146f0
Nathan Reyes <nathan.reyes.privat@gmail.com>
2020-08-14 18:41:26 +0200

    changed boilerplate

--- examples/Example app/app.js
+++ examples/Example app/app.js
@@ -2,8 +2,10 @@ class App extends Notic.Component { //The App component.
     
     html(){ //Method that generates html of the App component.
         var icon_1080 = `<img src = "img/icon-1080.png" id = "icon" />`;
-        var info = `<span style="font-size: 30px; opacity: 0.7;"> Source is located in /app.js.<span>`;
-        return  `${icon_1080} ${info}`;     //Returns the generated html.
+        var info = `<span id = "info"> Source is located in /app.js.</span>`;
+        var actionLink = '<a id = "action" href="https://github.com/Nathan2474/notic.js"> View on Github </a>';
+        
+        return  `${icon_1080} ${info} <br/><br/> ${actionLink}`;     //Returns the generated html.
     }
 
 }
