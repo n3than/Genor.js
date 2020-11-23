2c9dde97a65d50384ebfc41b139925b4d92a05c1
Nathan Reyes <nathan.reyes.privat@gmail.com>
2020-11-23 19:47:27 +0100

    📰New chat app
    This chat app will improve how notic will work. Specifically interactivity and updates.

---
+++ examples/chat app/app.js
@@ -0,0 +1,19 @@
+class App extends notic.Component {
+
+    html(){ 
+        return `<button onClick = "message = new Message('Chat');"> new </button>
+                    <div id="Chat"></div>`;
+    }
+
+}
+
+class Message extends notic.Component {
+    
+    html(){ 
+        return `helloworld!`;
+    }
+
+}
+
+var app = new App();
+notic.initialize();
