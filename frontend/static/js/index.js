97995472d1b2daece2845ef436f33f5c91fcf187
git-nathan <nathan.reyes.privat@gmail.com>
2020-12-13 05:06:57 +0100

    👷‍♀️ Creating project from scratch

---
+++ frontend/static/js/index.js
@@ -0,0 +1,27 @@
+const router = async () => {
+    const routes = [
+        {path: '/', view: ()=> console.log('Viewing Dashboard')},
+        {path: '/posts', view: ()=> console.log('Viewing Posts')},
+        {path: '/settings', view: ()=> console.log('Viewing Settings')}
+    ];
+    
+    const potentialMatches = routes.map(route => {
+        return {
+          route: route,
+          isMatch: route.path === location.pathname
+        };
+    });
+
+    let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
+    if(!match){
+        match = routes[0];
+        match.isMatch = true;
+        location.pathname = match.path;
+    }
+    console.log(match);
+    console.log(potentialMatches);
+};
+
+document.addEventListener('DOMContentLoaded', ()=> {
+    router();
+})
